7c7ea08a199455ef5b837b59754a502cc2b50cb7
Gustavo Inácio <gustavo.user0605@gmail.com>
2024-09-28 10:43:54 -0300

    olha só

--- Maratona/aula6/problema1329.cpp
+++ Maratona/aula6/problema1329.cpp
@@ -1,1 +1,26 @@
-ECHO est  ativado.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    while (cin >> n && n != 0) {
+        int mary = 0, john = 0;
+
+        for (int i = 0; i < n; i++) {
+            int c;
+            cin >> c;
+
+            if (c == 0) {
+                mary += 1;
+            } else {
+                john += 1;
+            }
+        }
+
+        cout << "Mary won " << mary << " times and John won " << john << " times" << endl;
+    }
+
+    return 0;
+}
